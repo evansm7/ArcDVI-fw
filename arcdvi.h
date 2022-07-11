134df0c0b4536139dce2bce81b8d5689872c32ab
Matt Evans <matt@ozlabs.org>
2022-07-11 11:02:51 +0100

    Add project board-specific config file

---
+++ arcdvi.h
@@ -0,0 +1,14 @@
+/* Board definition file for ArcDVI
+ *
+ * There's not much to do, as there are no LEDs or UARTs to steer towards.
+ * Flash also works fine with the default settings.
+ *
+ */
+
+#ifndef _ARCDVI_H
+#define _ARCDVI_H
+
+/* Grr, this always pisses me off.  Switch it the hell off: */
+#define PICO_STDIO_USB_DEFAULT_CRLF     false
+
+#endif
